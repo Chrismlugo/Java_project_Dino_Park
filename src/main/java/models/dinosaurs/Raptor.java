c770454d8396368e73007774765812f610966cad
Chris Murphy <_chrism_@live.com>
2018-03-23 12:10:22 +0000

    added Raptor

---
+++ src/main/java/models/dinosaurs/Raptor.java
@@ -0,0 +1,15 @@
+package models.dinosaurs;
+
+import models.Dino;
+import models.Enums.SpeciesType;
+import models.paddocks.Paddock;
+
+public class Raptor extends Dino {
+
+    public Raptor() {
+    }
+
+    public Raptor(String name, SpeciesType species, Paddock paddock) {
+        super(name, species, paddock);
+    }
+}
